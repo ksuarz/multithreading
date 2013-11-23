542e75defed2d2fa6f314c5479deca807881634f
indraneel <indraneelpurohit@gmail.com>
2013-11-23 17:54:08 -0500

    fixing some type-related errors

--- bookorder.c
+++ bookorder.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "queue.h"
 #include "list.h"
@@ -14,11 +15,11 @@ static queue_t *queues[3];
 int is_file(char *filepath) {
     FILE *file = fopen(filepath, "r");
     if (file) {
-	fclose(file);
-	return 1;
+        fclose(file);
+        return 1;
     }
     else {
-	return 0;
+        return 0;
     }
 }
 
@@ -54,28 +55,56 @@ void *producer_thread(void *args) {
  */
 database_t *setup_database(char *filepath) {
     FILE *database; 
-    char *entry, *lineptr;
+    char *entry, *lineptr, *name;
+    int customer_id, i;
+    float credit_limit;
     size_t len;
     ssize_t read;
-    database_t *returnDatabase = NULL;
+    database_t *returnDatabase = database_create();
+    customer_t *newCustomer = NULL;
+
     //make sure filepath is referencing a valid file
     if (is_file(filepath) == 0) {
-	fprintf(stderr, "Error: %s is not a valid filepath\n");
-	return returnDatabase;
+        fprintf(stderr, "Error: %s is not a valid filepath\n");
+        return returnDatabase;
     }
 
     database = fopen(filepath, "r");
 
-    lineptr = NULL;	
+    lineptr = NULL;     
     len = 0;
 
     while ((read = getline(&lineptr, &len, database)) != -1) {
-	while ((entry = strtok(NULL, "\n")) != NULL) {
-	    printf("line: %s\n", entry);
-	}
-    }
-    free(lineptr);
+        newCustomer = NULL;
+        for (i=0; i<3; i++) {
 	     
+	    if (i == 0) {
+		if((entry = strtok(lineptr, "|")) != NULL) {
+		    name = (char *) malloc(strlen(entry) + 1);
+		    strcpy(name, entry);
+		}
+	    }
+	    else if (i== 1) {
+		if ((entry = strtok(NULL, "|")) != NULL) {
+		    customer_id = atoi(entry); 
+		}
+	    }
+	    else if (i == 2) {
+		if ((entry = strtok(NULL, "|")) != NULL) {
+		    credit_limit = atof(entry);
+		}
+	    }
+	    else {
+		break;
+	    }
+        }
+        
+        newCustomer = customer_create(name, customer_id, credit_limit);
+        database_add_customer(returnDatabase, newCustomer);
+
+    }
+    free(newCustomer);
+    free(lineptr);
     fclose(database);
     return returnDatabase;
 }
@@ -84,12 +113,16 @@ database_t *setup_database(char *filepath) {
 int main(int argc, char **argv) {
     //There should be three arguments
     if (argc != 4) {
-	fprintf(stderr, "Error: wrong number of arguments\n");
-	print_usage();
-	return;
+        fprintf(stderr, "Error: wrong number of arguments\n");
+        print_usage();
+        return;
     }
 
     //Set up customer database from file
     database_t *customerDatabase = setup_database(argv[1]);
+
+    //spawn producer thread
+    
+    //spawn consumer thread
 
 }
